e59a0a4d812a4095adf48ea13bff33d075b00392
masudrana08 <rana028511@gmail.com>
2022-08-10 02:03:13 +0600

    Media Reaction - like, unlike (one user one reaction)

--- back/Controllers/MediaController/controlReaction.js
+++ back/Controllers/MediaController/controlReaction.js
@@ -1,7 +1,30 @@
 const MediaModel = require("../../Models/MediaModel")
+const uniqueArray = require("../../Utilities/uniqueArray")
 
 const controlReaction = (req, res) =>{
-  const {reactionType, id,  } = req.body
+  const {type, videoId } = req.body
+  if(req.user && req.user.username){
+    MediaModel.findOne({videoId})
+    .then(dbres=>{
+      // const reactions = [...dbres.like, ...dbres.unlike]
+      // if(reactions.indexOf(req.user.username)<0){
+       
+      // }
+      if(type=='like'){
+        dbres.like = uniqueArray([...dbres.like, req.user.username])
+        dbres.unlike = dbres.unlike.filter(i=>i != req.user.name)
+        dbres.save()
+      }
+      if(type=='unlike'){
+        dbres.unlike = uniqueArray([...dbres.unlike, req.user.username])
+        dbres.like = dbres.like.filter(i=>i != req.user.name)
+        dbres.save()
+      }
+      res.send(JSON.stringify({status:'ok'}))
+    })
+  }else{
+    res.status(401).send(JSON.stringify({status:'Unauthorized'}))
+  }
 
 }
 module.exports = controlReaction
