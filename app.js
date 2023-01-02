a0ef3c34e7489dcb4be9f4ffd6a8584b67cc4c05
masudrana08 <rana028511@gmail.com>
2023-01-02 17:55:02 +0600

    package change

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.listen(PORT, function () {
     console.log("Listening on port "+PORT);
 });
 
-mongoose.connect('mongodb://localhost:27017/streaming', ()=>{
+mongoose.connect(process.env.DB, ()=>{
     console.log('Database  Connected');
 })
